a18d468e989cc986fa43d9b615f2f3f962530238
biel0209 <marcus.melo@dcomp.ufs.br>
2021-05-18 16:25:27 -0300

    primeira versao

--- BibliotecaSW.h
+++ BibliotecaSW.h
@@ -1,7 +1,59 @@
 #ifndef BIBLIOTECASW_H
 #define BIBLIOTECASW_H
 
+typedef struct noFuncionario{
+    char nome[50];
+    int id;
+    int idade;
+    struct noFuncionario *prox;
+}NoFuncionario;
 
-//CABEÇALHO
+typedef struct noCarro{
+    char placa[6];
+    struct noCarro *prox;
+}NoCarro;
+
+
+void cadastrarFuncionarios(NoFuncionario *headFila, 
+    NoFuncionario *tailFila, char *nome, int id, int idade);
+void imprimirFila(NoFuncionario *headFila);
+
 
 #endif
+
+
+void cadastrarFuncionarios(NoFuncionario *headFila, 
+    NoFuncionario *tailFila, char *nome, int id, int idade)
+{
+    NoFuncionario *novo = malloc(sizeof(NoFuncionario));
+    if (novo != NULL){
+        strcpy(novo->nome, nome);
+        novo->id = id;
+        novo->idade = idade;
+        novo->prox = NULL;
+
+        if (headFila == NULL){
+            headFila = novo;
+        }else{
+            tailFila->prox = novo;
+        }
+        tailFila = novo;
+    }else{
+        printf("Memoria indisponivel\n");
+    }
+}
+
+void imprimirFila(NoFuncionario *headFila)
+{
+    if (headFila == NULL){
+        printf("A fila esta vazia\n");
+    }else{
+        printf("----------FILA FUNCIONARIOS---------\n");
+        while(headFila != NULL){
+            printf("Funcionario %d: Nome: %s\tIdade: %d\n", headFila->id, headFila->nome ,headFila->idade);
+            headFila = headFila->prox;
+        }
+    }
+}
+
+
--- SWEstacionamento.c
+++ SWEstacionamento.c
@@ -1,10 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "BibliotecaSW.h"
+#define qtdFuncionarios 3
+
+//TODO: NUMERO DE FUNCIONARIOS: 3         NUMERO TOTAL DE CARROS: 9
 
 int main()
 {
-    
-    
+    NoCarro *topo = NULL;
+    NoFuncionario *headFila = NULL;
+    NoFuncionario *tailFila = NULL;
+
+
+    char nome[50]; int idade;
+    for (int i=0; i<qtdFuncionarios; i++){
+        printf("Digite o nome do funcionario: ");
+        scanf("%s",nome);
+        printf("Digite a idade do funcionario: ");
+        scanf("%d",&idade);
+        cadastrarFuncionarios(headFila, tailFila, nome,(i+1),idade);
+    }
+
+    imprimirFila(headFila);
+
     return 0;
 }
